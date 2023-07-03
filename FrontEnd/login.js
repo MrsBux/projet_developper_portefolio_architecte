9c5198a9ab681d1794da4c16b220e3e36abcd261
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2023-07-03 15:39:53 +0200

    partie 1 fonctionnalité page de connextion

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -18,17 +18,17 @@ const labelEmail = document.createElement("label");
 labelEmail.innerText = "E-mail";
 organisationForm.appendChild(labelEmail);
 
-const inputEmail = document.createElement("input");
-inputEmail.innerText = "E-mail";
-organisationForm.appendChild(inputEmail);
+const email = document.createElement("input");
+email.innerText = "E-mail";
+organisationForm.appendChild(email);
 
 const labelMdp = document.createElement("label");
 labelMdp.innerText = "Mot de passe";
 organisationForm.appendChild(labelMdp);
 
-const inputMdp = document.createElement("input");
-inputMdp.innerText = "Mot de passe";
-organisationForm.appendChild(inputMdp);
+const password = document.createElement("input");
+password.innerText = "Mot de passe";
+organisationForm.appendChild(password);
 
 const buttonConnexion = document.createElement("submit");
 buttonConnexion.classList.add("connexion");
@@ -38,3 +38,29 @@ organisationForm.appendChild(buttonConnexion);
 const mdpOublie = document.createElement("a");
 mdpOublie.innerText = "Mot de passe oublié";
 sectionLogIn.appendChild(mdpOublie);
+
+const errorLogIn = document.createElement("p");
+errorLogIn.innerText = "Mauvaise combinaison";
+
+buttonConnexion.addEventListener("click", async function () {
+  const jeuMdpPassword = [email.value, password.value];
+  console.log(jeuMdpPassword);
+  const responsemdp = await fetch("http://localhost:5678/api/users/login");
+  console.log(responsemdp);
+});
+//   array.forEach(inputEmail, inputMdt => {
+
+//   }); {
+
+//
+// const responsemdp = await fetch("http://localhost:5678/api/users/login");
+// // const responseMDT = await responsemdp.json();
+// console.log(responsemdp);
+// if (responsemdp.status === 200) {
+//   console.log("Bien joué");
+// } else {
+//   // return "Mauvaise combinaison";
+//   console.log("Mauvaise combinaison");
+// }
+
+// (localStorage.clear()) et d'utiliser la méthode location.reload()
