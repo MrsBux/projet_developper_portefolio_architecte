68c8c69bddd94de1fd4c2499b0cd9f8be4b95293
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2023-07-24 16:30:53 +0200

    commentaire page login de connexion

--- p3frontend/FrontEnd/login.js
+++ p3frontend/FrontEnd/login.js
@@ -13,11 +13,13 @@ const organisationForm = document.createElement("fieldset");
 organisationForm.innerText = "";
 formulaire.appendChild(organisationForm);
 
+// titre formulaire
+
 const legend = document.createElement("legend");
 legend.innerText = "Log In";
 organisationForm.appendChild(legend);
 
-// zone input email et mot de passe
+// zone label/ input email et mot de passe
 
 const labelEmail = document.createElement("label");
 labelEmail.innerText = "E-mail";
@@ -42,7 +44,7 @@ buttonConnexion.classList.add("connexion");
 buttonConnexion.innerText = "Se connecter";
 organisationForm.appendChild(buttonConnexion);
 
-// lien mot de passe oublié
+// lien mot de passe oublié NON FONCTIONNEL
 
 const mdpOublie = document.createElement("a");
 mdpOublie.innerText = "Mot de passe oublié";
@@ -50,15 +52,16 @@ sectionLogIn.appendChild(mdpOublie);
 
 // Mise en fonction du formulaire --------------------------------------------------------
 
-const errorLogIn = document.createElement("p");
-errorLogIn.innerText = "Mauvaise combinaison";
-
+// ajout d'un event listener sur le bouton de connexion
 buttonConnexion.addEventListener("click", async function () {
+  // stockage d'un jeu email/mdp rentrés par l'utilisateur par un objet
   const jeuMdpPassword = {
     email: email.value,
     password: password.value,
   };
   console.log(jeuMdpPassword);
+
+  //stockage de la réponse de la requete dans une constante
   const responsemdp = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     body: JSON.stringify(jeuMdpPassword),
@@ -67,18 +70,23 @@ buttonConnexion.addEventListener("click", async function () {
     },
   });
 
+  //traduction de la réponse en json
   const responseBody = await responsemdp.json();
   console.log(responseBody);
-  debugger;
 
+  // condition IF pour succès ou echec de la connexion
+
+  // si reponse de la requete = 200 alors c'est un sccès
   if (responsemdp.status === 200) {
-    console.log("Bien joué");
-
+    // stockage du token en local
     window.localStorage.setItem("token", responseBody.token);
+
+    //renvoie vers la page d'accueil en mode login au bout de 100ms
     setTimeout(function () {
       window.location.href = "index.html";
     }, 100);
   } else {
+    // sinon c'est un echec et alors renvoie d'une alerte
     alert("Mauvaise combinaison Email / Mot de passe");
   }
 });
